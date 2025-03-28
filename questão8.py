a3c11c709310c118efabc33f79caa043eacb3164
pouopan <renanzinholoko66t@gmail.com>
2025-03-28 10:10:27 -0300

    Create questão8.py

---
+++ questão8.py
@@ -0,0 +1,3 @@
+temperatura_celsius = float(input("Digite a temperatura em Celsius: "))
+temperatura_fahrenheit = (temperatura_celsius * 9/5) + 32
+print(f'A temperatura em Fahrenheit é: {temperatura_fahrenheit}°F')
