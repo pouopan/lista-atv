6f5ee7fa07f998d062e03e330ac6ee9c0e4b1804
pouopan <renanzinholoko66t@gmail.com>
2025-03-28 10:11:47 -0300

    Create questão11.py

---
+++ questão11.py
@@ -0,0 +1,6 @@
+usuario = input("Digite o nome de usuário: ")
+senha = input("Digite a senha: ")
+if usuario == senha:
+    print("O nome de usuário e a senha não podem ser iguais.")
+else:
+    print("Login realizado com sucesso.")
